a93a24b57ae1ad95dc3b70cf2abd5b6e9e912c04
francienevaz <fran.vfraga@gmail.com>
2023-07-06 21:02:05 -0300

    updated

--- index.js
+++ index.js
@@ -1,3 +1,31 @@
 const input = document.querySelector("#search");
 const navMenu = document.querySelector("[data-type='nav-menu']");
+const details = Array.from(navMenu.querySelectorAll("details"));
+const lis = Array.from(navMenu.querySelectorAll("details li")) ;
+const liToggle = Array.from(navMenu.querySelectorAll("li .toggle"))
 
+input.addEventListener("input", function() {
+    const str = this.value;
+
+    if(str) {
+        filterData(str)
+    } else {
+        showAllItems()
+    }
+})
+
+function showAllItems() {
+    lis.forEach(li => li.classList.remove('hide'))
+}
+
+function filterData(str) {
+    showAllItems()
+    liToggle.forEach( liToggle => {
+        const details = liToggle.querySelector("details");
+        const summary = liToggle.querySelector("summary");
+
+        if (summary.textContent.toLowerCase().includes(str.toLowerCase())) {
+            details.setAttribute("open", "")
+        }
+    })
+}
